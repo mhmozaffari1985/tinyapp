4ef15b72575fa25a1a16d124b4673d010b2713f2
mh.mozaffari <mh.mozaffari1985@gmail.com>
2021-07-06 14:58:24 -0400

    Final w3d1

--- express_server.js
+++ express_server.js
@@ -2,11 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
 };
 
 // app.get("/", (req, res) => {
@@ -29,22 +32,46 @@ app.listen(PORT, () => {
 //   const a = 1;
 //   res.send(`a = ${a}`);
 //  });
- 
+
 //  app.get("/fetch", (req, res) => {
 //   res.send(`a = ${a}`);
 //  });
 
 app.get("/hello", (req, res) => {
-  const templateVars = { greeting: 'Hello World!' };
+  const templateVars = { greeting: "Hello World!" };
   res.render("hello_world", templateVars);
 });
 
- app.get("/urls", (req, res) => {
+app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: "http://google.com" };
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+  };
   res.render("urls_show", templateVars);
 });
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+
+app.post("/urls", (req, res) => {
+  urlDatabase[generateRandomString()] = req.body.longURL;
+  //console.log(req.body); // Log the POST request body to the console
+  //res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  res.redirect("/urls");
+});
+
+const generateRandomString = function() {
+  return Math.random.toString(36).substring(2,8);
+};
